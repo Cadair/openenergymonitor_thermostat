ccfaae5d2a8766a848fe2f897f57fd2f152b9247
Stuart Mumford <stuart@cadair.com>
2017-01-21 18:49:15 +0000

    Basic functionality and docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,9 +29,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-]
+extensions = ['sphinx_automodapi.automodapi', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'openenergymonitor_thermostat'
+project = 'oemthermostat'
 copyright = '2016, Stuart Mumford'
 author = 'Stuart Mumford'
 
